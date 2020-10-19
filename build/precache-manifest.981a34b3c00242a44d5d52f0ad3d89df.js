5073b44766b6ac5f89c18d5bb2c723cd4643d0eb
unknown <vikrant.sharma1>
2020-10-19 11:54:54 +0530

    build pushed

---
+++ build/precache-manifest.981a34b3c00242a44d5d52f0ad3d89df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e72dd9394af897495a4d239041cecaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebf1ea2d5ce4af5cb4d3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "996a43767344ca79e196",
+    "url": "/static/js/2.2f6b12e0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.2f6b12e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf1ea2d5ce4af5cb4d3",
+    "url": "/static/js/main.86730fb5.chunk.js"
+  },
+  {
+    "revision": "2b1d44a27f8315c7595d",
+    "url": "/static/js/runtime-main.b514ed92.js"
+  }
+]);
